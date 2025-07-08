e6f060f8db8a70d3680b6055c0397613fc2988a3
suraj <rockad23@gmail.com>
2025-07-08 21:18:01 +0530

    project add

---
+++ MyPythonProject/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template_string
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # return render_template_string("<h1>{{ msg }}</h1>", msg="Hello from Python!")
+    return render_template("index.html", msg="Welcome to My Stylish Flask Website!")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
